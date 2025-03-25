abb6c1484d37b87c5e3ae26a9234aa13a92e61cf
RedDeadDepresso <94017243+RedDeadDepresso@users.noreply.github.com>
2025-03-25 18:02:50 +0000

    feat: add about page and navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { Navbar } from "./components/Navbar";
+import { Navbar } from "./components/navbar";
+import { About } from "./components/about";
 
 function App() {
   return (
     <>
       <Navbar />
+      <About />
     </>
   );
 }
